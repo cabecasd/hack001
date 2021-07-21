411ac91016bdeb03b1509d5ef953c3b64fbceed8
Rafael Alegre <sopofdpdomv4@gmail.com>
2021-07-21 11:51:42 +0100

    login

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,35 @@
 import './App.css';
+import {
+  BrowserRouter,
+  Switch,
+  Route
+} from "react-router-dom";
+import InitialPage from './InitialPage';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <div>
+        </div>
+        <div>
+
+          <Switch>
+            
+            <Route path="/profile">
+              <p>perfil</p>
+            </Route>
+
+            <Route path="/home">
+              <p>homepage</p>
+            </Route>
+            <Route path="/">
+              <InitialPage />
+            </Route>
+          </Switch>
+        </div>
+
+      </BrowserRouter>
     </div>
   );
 }
