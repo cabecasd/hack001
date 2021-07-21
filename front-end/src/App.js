b8d6c7640d64208d7b71f72b604d0ebfcc792571
Fimbras <fimbras@gmail.com>
2021-07-21 11:57:27 +0100

    inicio navbar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Navbar from './components/HomePage';
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      
+      <Route exact path="/home" children={<Navbar />} />
     </div>
   );
 }
